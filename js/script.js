b373f4e1962a40a305b0aa1f1235ed492c0fc486
haryadt <145812740+haryadt@users.noreply.github.com>
2023-10-03 07:43:49 -0700

    Hide job role text box when "other" is not selected in the job role dropdown list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 // Focus on the name field on load
 const nameField = document.getElementById("name");
 nameField.focus();
+
+//Hide "other job role" when "other" is not selected in the job role dropdown list
+const jobRoleDropdown = document.getElementById("title");
+const otherJobRoleText = document.getElementById("other-job-role");
+
+otherJobRoleText.style.display = "none";
+
+jobRoleDropdown.addEventListener("change", (event) => {
+    if(event.target.value === "other") {
+        otherJobRoleText.style.display = "block";
+    } else {
+        otherJobRoleText.style.display = "none";
+    }
+});
